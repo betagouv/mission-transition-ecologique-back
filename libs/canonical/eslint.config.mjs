19eb48015c77ad9227b210d9ac9233f23fe808fd
ttdm <thibaut.tezenas@gmail.com>
2026-06-18 13:15:45 +0200

    feat: simplify config files

--- libs/canonical/eslint.config.mjs
+++ libs/canonical/eslint.config.mjs
@@ -1,4 +1,9 @@
 // @ts-check
 import baseConfig from '../../eslint.config.mjs'
 
-export default [...baseConfig]
+export default [
+  ...baseConfig,
+  {
+    ignores: ['**/vite.config.*.timestamp*', '**/vitest.config.*.timestamp*'],
+  },
+]
--- libs/canonical/vitest.config.ts
+++ libs/canonical/vitest.config.ts
@@ -1,12 +1,8 @@
 import { defineConfig } from 'vitest/config'
-import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  plugins: [tsconfigPaths()],
   test: {
     name: '@tee-backoffice/canonical',
-    environment: 'node',
     globals: true,
-    include: ['src/**/*.spec.ts'],
   },
 })
